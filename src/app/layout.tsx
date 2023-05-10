5b3f3b59a5088be5e81e5f99dc740745294d5f77
sjyoung428 <sjy8267@naver.com>
2023-05-10 20:31:32 +0900

    fix: layout style

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={openSans.className}>
-      <body className="w-full max-w-screen-xl overflow-auto mx-auto">
+      <body className="w-full overflow-auto bg-neutral-50">
         <AuthContext>
           <header className="sticky top-0 bg-white z-10 border-b">
-            <Navbar />
+            <div className="max-x-screen-xl mx-auto">
+              <Navbar />
+            </div>
           </header>
-          <main className="w-full flex justify-center bg-neutral-50 min-h-full">
+          <main className="w-full flex justify-center">
             <SWRConfigContext>{children}</SWRConfigContext>
           </main>
         </AuthContext>
