8b29d23a37ab0ee232e85672c73475f6ef8f2a1c
sjyoung428 <sjy8267@naver.com>
2023-05-10 17:44:44 +0900

    fix: image url

--- src/service/posts.ts
+++ src/service/posts.ts
@@ -1,10 +1,11 @@
-import { client } from "./sanity";
+import { SimplePost } from "~/models/post";
+import { client, urlFor } from "./sanity";
 
 const simplePostProjection = `
   ...,
   "username": author->username,
   "userImage": author->image,
-  "image" : photo,
+  "image": photo,
   "likes": likes[]->username,
   "text" : comments[0].comment,
   "comments": count(comments),
@@ -13,9 +14,15 @@ const simplePostProjection = `
 `;
 
 export const getFollowingPostsOf = async (username: string) => {
-  return client.fetch(`
+  return client
+    .fetch(
+      `
     *[_type == "post" && author->username == "${username}"
      || author._ref in *[_type == "user" && username == "${username}"].following[]._ref] 
      | order(_createdAt desc) {${simplePostProjection}}
-  `);
+  `
+    )
+    .then((posts: SimplePost[]) =>
+      posts.map((post) => ({ ...post, image: urlFor(post.image) }))
+    );
 };
--- src/service/sanity.ts
+++ src/service/sanity.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@sanity/client";
+import ImageUrlBuilder from "@sanity/image-url";
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
   projectId: process.env.SANITY_PROJECT_ID,
@@ -7,3 +9,11 @@ export const client = createClient({
   apiVersion: "2023-04-17", // use current date (YYYY-MM-DD) to target the latest API version
   token: process.env.SANITY_SECRET_TOKEN, // Only if you want to update content with the client
 });
+
+// https://www.sanity.io/docs/image-url
+
+const builder = ImageUrlBuilder(client);
+
+export const urlFor = (source: SanityImageSource) => {
+  return builder.image(source).width(800).url();
+};
